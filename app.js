4a54233f2355276023756c37633d737c7731212c
Zgerace <zach@Zacharys-MacBook-Pro.local>
2017-11-02 15:29:22 -0400

    Checking App.js file

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var mongoose = require('mongoose');
 var app = express();
 
 app.use(express.static('public'))
+app.get('/', function(req, res){
+    res.send('Testing World')
+})
 
-app.listen(8080, function(){
-        console.log('Server started on port 8080');
-});
+
+app.listen(8080, '172.31.87.197');
