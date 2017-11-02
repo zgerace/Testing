a2f6bfd3c90e8b6b007b90a68b04b3336132b5f6
Zgerace <zach@Zacharys-MacBook-Pro.local>
2017-11-02 16:25:24 -0400

    app.js update still not working

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-app.use(express.static('public'))
-app.get('/', function(req, res){
-    res.send('Testing World')
-})
+app.use(express.static('public'));
 
 
 app.listen(8080, '172.31.87.197');
